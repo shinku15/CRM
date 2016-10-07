620001e359dc1bd2ef833fbe25776bbb83f54b22
shinku <shrinkhala@classpro.in>
2016-10-07 12:05:32 +0530

    email changed

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -40,7 +40,7 @@ Rails.application.configure do
       :address              => "smtp.gmail.com",
       :port                 => 587,
       :domain               => "gmail.com",
-      :user_name            => "shrinkhala.mishra15@gmail.com",
+      :user_name            => "shrinkhala@classpro.in",
       :password             => "priyagupta",
       :authentication       => :plain,
       :enable_starttls_auto => true
